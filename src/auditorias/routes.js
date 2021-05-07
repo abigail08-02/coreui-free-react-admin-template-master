c4e81b2e1c99d41f67430dbb455fab1361bddf59
abigail <abigailguzman.08.02@gmail.com>
2021-05-07 08:34:10 -0500

    Creo uniones y trabajo en asociaciones

--- src/auditorias/routes.js
+++ src/auditorias/routes.js
@@ -5,6 +5,7 @@ const Dashboard = React.lazy(() => import('../views/dashboard/Dashboard'));
 const Users = React.lazy(() => import('../views/users/Users'));
 const User = React.lazy(() => import('../views/users/User'));
 const Iglesias = React.lazy(() => import('./iglesias/Iglesias'));
+const Uniones = React.lazy(() => import('./uniones/Uniones'));
 const AddEditIglesia = React.lazy(() => import('./iglesias/AddEditIglesia'));
 
 const routes = [
@@ -13,7 +14,12 @@ const routes = [
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/users/:id', exact: true, name: 'User Details', component: User },
   { path: '/iglesias', exact: true, name: 'Iglesias', component: Iglesias },
-  { path: '/iglesias/add', exact: true, name: 'Añadir Iglesias', component: AddEditIglesia }
+  { path: '/iglesias/add', exact: true, name: 'Añadir Iglesias', component: AddEditIglesia },
+  { path: '/uniones', exact: true, name: 'Uniones', component: Uniones },
+  { path: '/uniones/add', exact: true, name: 'Añadir Uniones', component: AddEditUnion },
+  { path: '/asociaciones', exact: true, name: 'Asociaciones', component: Asociaciones },
+
 ];
+
 
 export default routes;
